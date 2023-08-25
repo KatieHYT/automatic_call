c7dbdb88ad81a9f22bcc7aecaa6c456f0c66e1da
hyt <yuting2727@gmail.com>
2023-08-25 21:00:32 +0000

    rm redundancy

--- server.py
+++ server.py
@@ -118,43 +118,6 @@ class CradleCaller(ChatAgent):
             response = self.init_phrase
         return response
 
-class TTSClient(ABC):
-    @abstractmethod
-    def text_to_mp3(self, text: str, output_fn: Optional[str] = None) -> str:
-        pass
-
-    def play_text(self, text: str) -> str:
-        tmp_mp3 = self.text_to_mp3(text)
-        tmp_wav = tmp_mp3.replace(".mp3", ".wav")
-        subprocess.call(["ffmpeg", "-hide_banner", "-loglevel", "error", "-y", "-i", tmp_mp3, tmp_wav])
-
-        wf = wave.open(tmp_wav, "rb")
-        audio = pyaudio.PyAudio()
-        stream = audio.open(
-            format=audio.get_format_from_width(wf.getsampwidth()),
-            channels=wf.getnchannels(),
-            rate=wf.getframerate(),
-            output=True,
-        )
-
-        data = wf.readframes(1024)
-        while data != b"":
-            stream.write(data)
-            data = wf.readframes(1024)
-
-        stream.close()
-        audio.terminate()
-
-    def get_duration(self, audio_fn: str) -> float:
-        popen = subprocess.Popen(
-            ["ffprobe", "-hide_banner", "-loglevel", "error", "-show_entries", "format=duration", "-i", audio_fn],
-            stdout=subprocess.PIPE,
-        )
-        popen.wait()
-        output = popen.stdout.read().decode("utf-8")
-        duration = float(output.split("=")[1].split("\r")[0])
-        return duration
-
 class TwilioCallSession:
     def __init__(self, ws, client: Client, remote_host: str, static_dir: str):
         self.ws = ws
