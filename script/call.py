79a6215112120b57faa1f64c6d89303553644d78
yth <yuting2727@gmail.com>
2023-08-30 18:33:07 +0000

    assign ngrok url when calling

--- script/call.py
+++ script/call.py
@@ -5,6 +5,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--call_to', type=str, required=True, help='e.g., +18668978264')
+    parser.add_argument('--ngrok_url', type=str, default=None, help='this will change, if you do not serve your own service, ask for it.')
     args = parser.parse_args()
 
     # Find your Account SID and Auth Token at twilio.com/console
@@ -13,7 +14,11 @@ if __name__ == '__main__':
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
     
-    remote_host_url = os.environ["REMOTE_HOST_URL"]
+    if args.ngrok_url is not None:
+        remote_host_url = args.ngrok_url
+        print(f"Using ngrok url from argparser: {remote_host_url}")
+    else:
+        remote_host_url = os.environ["REMOTE_HOST_URL"]
     
     call = client.calls.create(
                             #to='+14156054429',
